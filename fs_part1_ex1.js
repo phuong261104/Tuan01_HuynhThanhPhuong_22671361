75df370a53f02cfb618a0fcf2bdc7689cdf061e2
phuong261104 <22671361.phuong@student.iuh.edu.vn>
2025-01-08 10:08:40 +0700

    Finish JF Part1 _ Challenge 1

---
+++ fs_part1_ex1.js
@@ -0,0 +1,29 @@
+let markMass1 = 78;
+let markHeight1 = 1.69;
+let johnMass1 = 92;
+let johnHeight1 = 1.95;
+
+let markBMI1 = markMass1 / (markHeight1 ** 2);
+let johnBMI1 = johnMass1 / (johnHeight1 ** 2);
+
+let markHigherBMI1 = markBMI1 > johnBMI1;
+
+console.log("1:");
+console.log("Mark's BMI:", markBMI1);
+console.log("John's BMI:", johnBMI1);
+console.log("Mark has a higher BMI than John?", markHigherBMI1);
+
+let markMass2 = 95;
+let markHeight2 = 1.88;
+let johnMass2 = 85;
+let johnHeight2 = 1.76;
+
+let markBMI2 = markMass2 / (markHeight2 ** 2);
+let johnBMI2 = johnMass2 / (johnHeight2 ** 2);
+
+let markHigherBMI2 = markBMI2 > johnBMI2;
+
+console.log("\n2:");
+console.log("Mark's BMI:", markBMI2);
+console.log("John's BMI:", johnBMI2);
+console.log("Mark has a higher BMI than John?", markHigherBMI2);
